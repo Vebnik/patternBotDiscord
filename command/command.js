5b6403e8b43d35de6cd82cb1887b325f2c92d330
Vebnik(Nik) <vebnik111@gmail.com>
2022-04-17 20:36:52 +0300

    add first logic

---
+++ command/command.js
@@ -0,0 +1,11 @@
+function listenInteraction (inter) {
+
+
+
+}
+
+function listenMessage (msg) {
+
+}
+
+module.exports = {listenInteraction, listenMessage}
---
+++ startLogic/start.js
@@ -0,0 +1,20 @@
+const { listenInteraction, listenMessage } = require('../command/command.js')
+
+function StartApp () {
+
+	this.start = (client) => {
+		client.login(process.env.TOKEN)
+			.then(start => console.log(`App started: ${Date.now()}`))
+			.catch(err => throw err)
+	}
+
+	this.listenEvent = (client) => {
+		client.on('interactionCreate', inter => { listenInteraction(inter) })
+
+		client.on('messageCreate', message => {  })
+	}
+
+}
+
+
+module.exports = { StartApp }
